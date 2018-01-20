fda85aa76c5061781799a2fb0e26b58f68ebcd84
Perconey <perconey@gmail.com>
2018-01-20 19:02:23 +0100

    Added project structure and example strings for further analysis

---
+++ StringEx/StringEx Tests/StringTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringEx_Tests
+{
+    [TestClass]
+    public class StringTests
+    {
+        private static string ts1 = "Hello and welcome to my code";
+        private static string ts2 = "Octahedron";
+        private static string ts3 = "I'm okay, and you're not";
+        private static string ts4 = "perconey@gmail.com";
+        private static string ts5 = "746582294";
+        private static string ts6 = "Hello! My phone number is 827112333 and email example@web.net";
+        private static string ts7 = "Mess,!jo@)!n04n0N!@)9ncala)()(@@[};'[].]]";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
