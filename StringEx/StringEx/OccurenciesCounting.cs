fbbb139b1b5046e2394558524ea22064a2adcc11
Perconey <perconey@gmail.com>
2018-01-21 23:06:27 +0100

    Added occurencies counting methods

---
+++ StringEx/StringEx/OccurenciesCounting.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace StringEx
+{
+    public static class OccurenciesCounting
+    {
+
+        /// <summary>
+        ///     Occurencies counting methods
+        /// </summary>
+
+
+        public static int WordsStandardCount(this string str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public static int UpperCaseCount(this string str)
+        {
+            int i = 0;
+            foreach (char c in str)
+                if(Char.IsUpper(c))
+                    i++;
+
+            return i;
+        }
+
+        public static int LowerCaseCount(this string str)
+        {
+            int i = 0;
+            foreach (char c in str)
+                if (Char.IsLower(c))
+                    i++;
+            return i;
+        }
+
+        public static int NumbersCount(this string str)
+        {
+            int i = 0;
+            foreach (char c in str)
+                if (Char.IsNumber(c))
+                    i++;
+            return i;
+        }
+
+        public static int SymbolsCount(this string str)
+        {
+            int i = 0;
+            foreach (char c in str)
+                if (Char.IsSymbol(c))
+                    i++;
+
+            return i;
+        }
+
+        public static int SurrogateCount(this string str)
+        {
+            int i = 0;
+            foreach (char c in str)
+                if (Char.IsSurrogate(c))
+                    i++;
+            return i;
+        }
+    }
+}
