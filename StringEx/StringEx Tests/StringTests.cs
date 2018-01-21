1eeeb5dbff0577460f81ce4ba07b9223ef53cb97
Perconey <perconey@gmail.com>
2018-01-21 23:58:46 +0100

    Case conversion

--- StringEx/StringEx Tests/StringTests.cs
+++ StringEx/StringEx Tests/StringTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringEx;
 
 namespace StringEx_Tests
 {
@@ -13,10 +15,13 @@ namespace StringEx_Tests
         private static string ts5 = "746582294";
         private static string ts6 = "Hello! My phone number is 827112333 and email example@web.net";
         private static string ts7 = "Mess,!jo@)!n04n0N!@)9ncala)()(@@[};'[].]]";
+        private static string ts8 = "You WILL be noticed by ME, or HIM!";
 
         [TestMethod]
         public void TestMethod1()
         {
+            Debug.WriteLine(ts1.ToCamelCase());
+            Debug.WriteLine("hello");
         }
     }
 }
---
+++ StringEx/StringEx/CaseConverting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEx
+{
+    public static class CaseConverting
+    {
+        public static string ToCamelCase(this string str)
+        {
+            str = str.ToLower();
+            int i = 0;
+            foreach (char c in str)
+            {
+                if (c == ' ')
+                {
+                    str = str.Remove(i, 1);
+                    try
+                    {
+                        str = str.Insert(i, Char.ToUpper(str[i]).ToString());
+                        str = str.Remove(i + 1, 1);
+                    }
+                    catch (Exception ex) { };
+                }
+                i++;
+            }
+            return str;
+        }
+
+    }
+}
