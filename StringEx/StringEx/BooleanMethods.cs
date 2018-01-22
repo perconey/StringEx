8f81d0743f7d17d0e79b04f46d13dedf73d5107d
Perconey <perconey@gmail.com>
2018-01-23 00:16:49 +0100

    Boolean Methods

---
+++ StringEx/StringEx/BooleanMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEx
+{
+    public static class BooleanMethods
+    {
+        public static bool IsCamelCase(this string str)
+        {
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsSymbol(str[i]) || Char.IsWhiteSpace(str[i]) || Char.IsUpper(str[0])) return false;
+            }
+            return true;
+        }
+    }
+}
